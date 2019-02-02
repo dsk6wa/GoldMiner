abeef3600f333a98b6f8273be28b19a7966de91e
dsk6wa <dsk6wa@virginia.edu>
2019-02-01 21:07:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+var saveGame = JSON.parse(localStorage.getItem("goldMinerSave"))
+
 var gameData = {
   gold: 0,
   goldPerClick: 1,
-  goldPerClickCost: 10
+  goldPerClickCost: 10,
+  lastTick: Date.now(),
 }
 
+function update(id, content) {
+  document.getElementById(id).innerHTML = content;
+}
 function mineGold() {
   gameData.gold += gameData.goldPerClick
-  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+  update("goldMined", format(gameData.gold,"scientific") + " Gold Mined")
 }
 
 function buyGoldPerClick() {
@@ -14,11 +20,42 @@ function buyGoldPerClick() {
     gameData.gold -= gameData.goldPerClickCost
     gameData.goldPerClick += 1
     gameData.goldPerClickCost *= 2
-    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Current Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+    update("goldMined", format(gameData.gold,"scientific") + " Gold Mined")
+    update("perClickUpgrade", "Upgrade Pickaxe (Current Level " + format(gameData.goldPerClick,"scientific") + ") Cost: " + format(gameData.goldPerClickCost,"scientific") + " Gold")
   }
 }
 
 var mainGameLoop = window.setInterval(function() {
-  mineGold()
+  diff = Date.now() - gameData.lastTick;
+  gameData.lastTick = Date.now()
+  gameData.gold += gameData.goldPerClick * (diff / 1000)
+  update("goldMined",format(gameData.gold,"scientific") + " Gold Mined")
 }, 1000)
+
+var saveGameLoop = window.setInterval(function(){
+  localStorage.setItem('goldMinerSave', JSON.stringify(gameData))
+}, 15000)
+
+function format(number, type) {
+    let exponent = Math.floor(Math.log10(number))
+    let mantissa = number / Math.pow(10, exponent)
+    if (exponent < 3) return number.toFixed(0)
+    if (type == "scientific") return mantissa.toFixed(2) + "e" + exponent
+    if (type == "engineering") return (Math.pow(10, exponent % 3) * mantissa).toFixed(2) + "e" + (Math.floor(exponent / 3) * 3)
+}
+
+function tab(tab){
+  // hide all your tabs, then show the one the user selected.
+  document.getElementById("mineGoldMenu").style.display = "none"
+  document.getElementById("shopMenu").style.display = "none"
+  document.getElementById(tab).style.display = "inline-block"
+}
+// go to a tab for the first time, so not all show
+tab('mineGoldMenu')
+
+if (saveGame !== null) {
+  if (typeof saveGame.gold !== "undefined") gameData.gold = saveGame.gold;
+  if (typeof saveGame.goldPerClick !== "undefined") gameData.goldPerClick = saveGame.goldPerClick;
+  if (typeof saveGame.goldPerClickCost !== "undefined") gameData.goldPerClickCost = saveGame.goldPerClickCost;
+  if (typeof saveGame.lastTick !== "undefined") gameData.lastTick = saveGame.lastTick;
+}
